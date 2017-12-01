d1630a1ac502fdfee2693423741b5b435838d78d
Kirimichan <mariko.ohtsuka@nnh.go.jp>
2017-12-01 15:21:12 +0900

    testtool edit

--- testtool/compare-bef.R
+++ testtool/compare-bef.R
@@ -1,13 +1,18 @@
 # 変更前ファイルの列を変更後と同一にした比較ファイルを出力する
-prtpath <- "C:/Users/MarikoOhtsuka/Documents/github/ptosh-csv"
+prtpath <- "C:/Users/MarikoOhtsuka/Desktop/HL14"
 diffpath <- paste0(prtpath, "/diff")
-diffeditpath <- paste0(prtpath, "/diffedit")
+
 constbef <- "_170821_1241.csv"
 constaft <- "_171027_1305.csv"
 # diffフォルダ内のファイル名を取得
 diff_list <- list.files(diffpath)
 kAllocation <- "allocation"
-
+kTrialTitle  <- "HL-14"
+# 出力フォルダが存在しなければ作成
+diffeditpath <- paste0(prtpath, "/diffedit")
+if (!(file.exists(diffeditpath))){
+  dir.create(diffeditpath)
+}
 
 
 for (i in 1:length(diff_list)){
@@ -32,11 +37,11 @@ for (i in 1:length(diff_list)){
     output_csv$最終更新日 <- input_csv$最終更新日
     input_csv <- input_csv[ ,colnames(input_csv) != "最終更新日"]
     # "試験名"
-    output_csv$試験名 <- input_csv$試験名
+    output_csv$試験名 <- kTrialTitle
     input_csv <- input_csv[ ,colnames(input_csv) != "試験名"]
     # "シート名"
     output_csv$シート名 <- NA
-    input_csv <- input_csv[ ,colnames(input_csv) != "シート名"]
+    input_csv <- input_csv[ ,colnames(input_csv) != "シート名英数字別名"]
     # "シート作成時施設名"
     output_csv$シート作成時施設名 <- input_csv$シート作成時施設名
     input_csv <- input_csv[ ,colnames(input_csv) != "シート作成時施設名"]
@@ -55,7 +60,7 @@ for (i in 1:length(diff_list)){
     output_csv$診察券番号 <- NA
     input_csv <- input_csv[ ,colnames(input_csv) != "診察券番号"]
     # "生年月日"
-    output_csv$生年月日 <- input_csv$生年月日
+    output_csv$生年月日 <- NA
     input_csv <- input_csv[ ,colnames(input_csv) != "生年月日"]
     # "診断日"
     # 空列にする
@@ -72,29 +77,29 @@ for (i in 1:length(diff_list)){
     output_csv$登録コード <- input_csv$登録コード
     input_csv <- input_csv[ ,colnames(input_csv) != "登録コード"]
     # "患者イニシャル"
-    output_csv$患者イニシャル <- input_csv$患者イニシャル
+    output_csv$患者イニシャル <- NA
     input_csv <- input_csv[ ,colnames(input_csv) != "患者イニシャル"]
     # "患者カナ"
-    output_csv$患者カナ <- input_csv$患者カナ
+    output_csv$患者カナ <- NA
     input_csv <- input_csv[ ,colnames(input_csv) != "患者カナ"]
     # "症例登録番号"
     output_csv$症例登録番号 <- input_csv$症例登録番号
     input_csv <- input_csv[ ,colnames(input_csv) != "症例登録番号"]
     # "性別"
-    output_csv$性別 <- input_csv$性別
+    output_csv$性別 <- NA
     input_csv <- input_csv[ ,colnames(input_csv) != "性別"]
     # "住所"
     # 空列にする
     output_csv$住所 <- NA
     input_csv <- input_csv[ ,colnames(input_csv) != "住所"]
     # "生死"
-    output_csv$生死 <- input_csv$生死
+    output_csv$生死 <- NA
     input_csv <- input_csv[ ,colnames(input_csv) != "生死"]
     # "死亡日"
-    output_csv$死亡日 <-  input_csv$死亡日
+    output_csv$死亡日 <-  NA
     input_csv <- input_csv[ ,colnames(input_csv) != "死亡日"]
     # "最終確認日"
-    output_csv$最終確認日 <-  input_csv$最終確認日
+    output_csv$最終確認日 <-  NA
     input_csv <- input_csv[ ,colnames(input_csv) != "最終確認日"]
     # "シート作成時施設コード"
     # "再入力依頼"
@@ -124,8 +129,7 @@ for (i in 1:length(diff_list)){
       output_csv <- cbind(output_csv,input_csv)
     }
     # csv出力
-    optnm <-  gsub(constbef, constaft, diff_list[i])
-    write.csv(output_csv, paste(diffeditpath, optnm, sep="/"), na='""', row.names=F)
+    write.csv(output_csv, paste(diffeditpath, diff_list[i], sep="/"), na='""', row.names=F)
 
   }
 }
